68912321168c7faccec4bd866889671b01c3ab6e
ndornseif <5382403+ndornseif@users.noreply.github.com>
2024-06-12 22:00:41 +0200

    Added counter encryption mode.

--- src/modes.rs
+++ src/modes.rs
@@ -34,22 +34,35 @@ fn ecb_decrypt(data: &mut Vec<[u8; 32]>, subkeys: &[[u8; 16]; 36]) {
 
 /// CTR encrypt a vec of blocks
 ///
-/// XORs both halfs of the nonce with a block counter to generate feistel cipher input.
-/// Adds nonce to the end of data vec. Nonce needs to be random to resist chosen-plaintext attacks.
-// fn ctr_encrypt(data: &mut Vec<[u8; 32]>, subkeys: &[[u8; 16]; 36], nonce: &[u8; 32]) {
-//     for (i, block) in data.iter_mut().enumerate() {
-//         let mut crypt_block: [u8; 32] = encrypt_block(, subkeys);
-//         crypt_block[0] = crypt_block[0] ^ block[0];
-//         crypt_block[1] = crypt_block[1] ^ block[1];
-//         *block = crypt_block;
-//     }
-// }
+/// Adds the block number to the nonce and encrypts this value to generate keystream.
+/// XORs data blocks with keystream.
+/// Nonce needs to be random to resist chosen-plaintext attacks.
+fn ctr_encrypt(data: &mut Vec<[u8; 32]>, subkeys: &[[u8; 16]; 36], nonce: &[u8; 32]) {
+    for (i, block) in data.iter_mut().enumerate() {
+        let mut crypt_block: [u8; 32] = crypt::encrypt_block(&utils::wrapping_add_256bit_u64(*nonce, i as u64), subkeys);
+        *block = utils::xor_256bit(crypt_block, *block);
+    }
+}
+
+/// CTR decrypt a vec of blocks
+///
+/// Adds the block number to the nonce and encrypts this value to generate keystream.
+/// XORs data blocks with keystream.
+/// Nonce needs to be random to resist chosen-plaintext attacks.
+fn ctr_decrypt(data: &mut Vec<[u8; 32]>, subkeys: &[[u8; 16]; 36], nonce: &[u8; 32]) {
+    for (i, block) in data.iter_mut().enumerate() {
+        let mut crypt_block: [u8; 32] = crypt::encrypt_block(&utils::wrapping_add_256bit_u64(*nonce, i as u64), subkeys);
+        *block = utils::xor_256bit(crypt_block, *block);
+    }
+}
 
 
 #[cfg(test)]
 mod unit_tests {
 	static TEST_KEY: [u8; 64] = [0x64, 0x54, 0x82, 0x64, 0xb3, 0x10, 0x1e, 0x0, 0x8b, 0xd8, 0xc, 0xb9, 0xf2, 0x7b, 0x8a, 0x89, 0xaa, 0x3b, 0x6d, 0x36, 0x1d, 0x47, 0x1f, 0x4d, 0xa8, 0xa6, 0x1f, 0xc2, 0x12, 0x66, 0x67, 0x7b, 0xb6, 0xf6, 0x11, 0x98, 0xad, 0x77, 0x66, 0x67, 0xbe, 0x5a, 0xbb, 0x9b, 0xef, 0xaa, 0x2a, 0x71, 0xdc, 0xad, 0x2c, 0x6a, 0xe5, 0xc8, 0x2b, 0xcf, 0xea, 0x2c, 0x9d, 0xe7, 0x1a, 0x83, 0x4a, 0xe7];
     static TEST_DATA_BLOCK: [u8; 32] = [0x80, 0xf, 0x6a, 0x56, 0x87, 0xda, 0xa4, 0xda, 0xed, 0x44, 0x47, 0xd9, 0xd1, 0x24, 0x8b, 0xff, 0x93, 0x84, 0xd8, 0xd6, 0x20, 0x4a, 0x5e, 0x27, 0x8a, 0x7b, 0x17, 0x39, 0x31, 0xeb, 0x6a, 0x3c];
+    const TEST_NONCE: [u8; 32] = [44, 7, 114, 184, 174, 250, 236, 1, 211, 183, 59, 179, 138, 241, 217, 132, 22, 138, 161, 0, 109, 159, 161, 210, 39, 239, 67, 99, 144, 233, 89, 12];
+
     use super::*;
 
 	#[test]
@@ -62,6 +75,19 @@ mod unit_tests {
         ecb_encrypt(&mut data, &subkeys);
         assert_ne!(intial_data, data);
         ecb_decrypt(&mut data, &subkeys);
+        assert_eq!(intial_data, data);
+    }
+
+    #[test]
+    /// Checks if the plaintext is correctly returned when encrypting and decrypting in CTR mode.
+    fn ctr_encrypt_decrypt() {
+    	let mut data = Vec::new();
+        data.push(TEST_DATA_BLOCK);
+        let intial_data = data.clone();
+        let subkeys = crypt::subkey_generation(&TEST_KEY);
+        ctr_encrypt(&mut data, &subkeys, &TEST_NONCE);
+        assert_ne!(intial_data, data);
+        ctr_decrypt(&mut data, &subkeys, &TEST_NONCE);
         assert_eq!(intial_data, data);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -38,6 +38,17 @@ pub fn xor_128bit(a: [u8; 16], b: [u8; 16]) -> [u8; 16] {
     (a0 ^ b0).to_be_bytes() 
 }
 
+/// Perform XOR on two 256bit values
+///
+/// Takes two 32 byte arrays and performs bitwise XOR. 
+pub fn xor_256bit(a: [u8; 32], b: [u8; 32]) -> [u8; 32] {
+    let mut result = [0u8; 32];
+    for i in 0..32 {
+        result[i] = a[i] ^ b[i];
+    }
+    result
+}
+
 /// Perform bit rotaion on 128bit value
 ///
 /// Takes in a 16 byte array and a shift amount.
@@ -56,6 +67,29 @@ pub fn right_rotate_128bit(a: [u8; 16], amount: u32) -> [u8; 16] {
     a0.rotate_right(amount).to_be_bytes()
 }
 
+/// Wrapping addition of 256bit byte array and u64
+///
+/// Perform wrapping addition between a 256bit number supplied as 32 byte array and a u64.
+pub fn wrapping_add_256bit_u64(a: [u8; 32], b: u64) -> [u8; 32] {
+    let mut result = [0u8; 32];
+    let mut carry = 0u16;
+
+    let b_bytes = b.to_le_bytes();
+    for i in 0..8 {
+        let sum = a[i] as u16 + b_bytes[i] as u16 + carry;
+        result[i] = sum as u8;
+        carry = sum >> 8;
+    }
+    // Propagate the carry to the remaining bytes
+    for i in 8..32 {
+        let sum = a[i] as u16 + carry;
+        result[i] = sum as u8;
+        carry = sum >> 8;
+    }
+    result.reverse();
+    result
+}
+
 #[cfg(test)]
 mod unit_tests {
     use super::*;
@@ -69,16 +103,16 @@ mod unit_tests {
     }
 
     #[test]
-    /// Test if wrapping_add_128bit works
+    /// Test if wrapping_add_128bit produces expected result.
     fn test_wrapping_add_128bit() {
-        const INPUT_A: [u8; 16] = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+        const INPUT_A: [u8; 16] = [0xff; 16];;
         const INPUT_B: [u8; 16] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xee];
         let rslt = wrapping_add_128bit(INPUT_A, INPUT_B);
         assert_eq!(rslt, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xed]);
     }
 
     #[test]
-    /// Test if xor_128bit works
+    /// Test if xor_128bit produces expected result.
     fn test_xor_128bit() {
         const INPUT_A: [u8; 16] = [0xb5, 0x7f, 0x43, 0xf4, 0xc9, 0x7d, 0x8d, 0xf1, 0x92, 0x67, 0xbd, 0x04, 0x2f, 0x6d, 0x07, 0x94];
         const INPUT_B: [u8; 16] = [0x39, 0x63, 0x7e, 0x8f, 0x0e, 0x26, 0x79, 0x5d, 0x9b, 0x88, 0xa3, 0x45, 0x23, 0xe6, 0xe5, 0x56];
@@ -87,7 +121,16 @@ mod unit_tests {
     }
 
     #[test]
-    /// Test if left_rotate_128bit works
+    /// Test if xor_256bit produces expected result.
+    fn test_xor_256bit() {
+        const INPUT_A: [u8; 32] = [0xb5, 0x7f, 0x43, 0xf4, 0xc9, 0x7d, 0x8d, 0xf1, 0x92, 0x67, 0xbd, 0x04, 0x2f, 0x6d, 0x07, 0x94, 0x39, 0x63, 0x7e, 0x8f, 0x0e, 0x26, 0x79, 0x5d, 0x9b, 0x88, 0xa3, 0x45, 0x23, 0xe6, 0xe5, 0x56];
+        const INPUT_B: [u8; 32] = [0xa7, 0x3b, 0x22, 0x4a, 0x12, 0x26, 0x0f, 0x37, 0x60, 0xf4, 0xe7, 0x74, 0xc4, 0xd0, 0xd8, 0xc0, 0x48, 0x49, 0x81, 0xc9, 0xb7, 0xaa, 0x08, 0xec, 0xaf, 0xcc, 0xdb, 0x17, 0xdd, 0x3f, 0x0d, 0x52];
+        let rslt = xor_256bit(INPUT_A, INPUT_B);
+        assert_eq!(rslt, [0x12, 0x44, 0x61, 0xbe, 0xdb, 0x5b, 0x82, 0xc6, 0xf2, 0x93, 0x5a, 0x70, 0xeb, 0xbd, 0xdf, 0x54, 0x71, 0x2a, 0xff, 0x46, 0xb9, 0x8c, 0x71, 0xb1, 0x34, 0x44, 0x78, 0x52, 0xfe, 0xd9, 0xe8, 0x04]);
+    }
+
+    #[test]
+    /// Test if left_rotate_128bit produces expected result.
     fn test_left_rotate_128bit() {
         const INPUT_A: [u8; 16] = [0xb5, 0x7f, 0x43, 0xf4, 0xc9, 0x7d, 0x8d, 0xf1, 0x92, 0x67, 0xbd, 0x04, 0x2f, 0x6d, 0x07, 0x94];
         let rslt = left_rotate_128bit(INPUT_A, 5);
@@ -95,10 +138,18 @@ mod unit_tests {
     }
 
     #[test]
-    /// Test if right_rotate_128bit works
+    /// Test if right_rotate_128bit produces expected result.
     fn test_right_rotate_128bit() {
         const INPUT_A: [u8; 16] = [0xb5, 0x7f, 0x43, 0xf4, 0xc9, 0x7d, 0x8d, 0xf1, 0x92, 0x67, 0xbd, 0x04, 0x2f, 0x6d, 0x07, 0x94];
         let rslt = right_rotate_128bit(INPUT_A, 5);
         assert_eq!(rslt, [0xa5, 0xab, 0xfa, 0x1f, 0xa6, 0x4b, 0xec, 0x6f, 0x8c, 0x93, 0x3d, 0xe8, 0x21, 0x7b, 0x68, 0x3c]);
+    }
+
+    #[test]
+    /// Test if wrapping_add_256bit_u64 produces expected result.
+    fn test_wrapping_add_256bit_u64() {
+        const INPUT_A: [u8; 32] = [0xff; 32];
+        let rslt = wrapping_add_256bit_u64(INPUT_A, 2u64);
+        assert_eq!(rslt, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
     }
 }
